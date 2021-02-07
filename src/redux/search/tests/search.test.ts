db222b08a0cb55d6d89945817819df507f83891e
nitin reddy <reddynitin3@gmail.com>
2021-02-08 02:26:23 +0530

    Added the tests for the search actions

--- src/redux/search/tests/search.test.ts
+++ src/redux/search/tests/search.test.ts
@@ -1,11 +1,11 @@
 import * as actions from '../searchActions';
-import { FETCH_REPOSITORIES_SUCCESS, FETCH_USERS_SUCCESS } from '../searchTypes'
+import { FETCH_REPOSITORIES_SUCCESS, FETCH_USERS_SUCCESS, CLEAR_USERS, CLEAR_REPOSITORIES } from '../searchTypes'
 
-describe('actions', () => {
-  it('should create an action to fetch the repositories', () => {
+describe("actions", () => {
+  it("should create an action to fetch the repositories", () => {
     const repositories = [
       { name: 'REX', id: 1 },
-      { login: 'Flutter', id: 2 },
+      { name: 'Flutter', id: 2 },
     ]
     const expectedAction = {
       type: FETCH_REPOSITORIES_SUCCESS,
@@ -14,7 +14,7 @@ describe('actions', () => {
     expect(actions.fetchRepositoriesSuccess(repositories)).toEqual(expectedAction)
   })
 
-  it('should create an action to fetch the users', () => {
+  it("should create an action to fetch the users", () => {
     const users = [
       { login: 'Nitin', id: 1 },
       { login: 'Jiya', id: 2 },
@@ -25,4 +25,23 @@ describe('actions', () => {
     }
     expect(actions.fetchUsersSuccess(users)).toEqual(expectedAction)
   })
+
+  it("should create an action to clear the users' data", () => {
+    const users: Array<object> = []
+    const expectedAction = {
+      type: CLEAR_USERS,
+      payload: users
+    }
+    expect(actions.clearUsers(users)).toEqual(expectedAction)
+  })
+
+  it("should create an action to clear the repositories' data", () => {
+    const repositories: Array<object> = []
+    const expectedAction = {
+      type: CLEAR_REPOSITORIES,
+      payload: repositories
+    }
+    expect(actions.clearRepositories(repositories)).toEqual(expectedAction)
+  })
+
 })
